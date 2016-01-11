2554371300832fd33f0fdbc7162b02ee6716c357
spdev101 <spdev101@live.com>
2016-01-11 18:23:18 +0530

    Added Angular2

---
+++ Personaa/Personaa.Add-InWeb/AddIn/Home/app.component.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="C:\Git Projects\Personaa\Personaa\Personaa.Add-InWeb\Scripts/angular2/angular2-all.umd.js" />
+(function (app) {
+    app.AppComponent =
+    ng.core.Component({
+        selector: 'personaa-app',        
+        templateUrl: 'Personaa.html'
+    })
+    .Class({
+        constructor: function () { }
+    });
+})(window.app || (window.app = {}));
